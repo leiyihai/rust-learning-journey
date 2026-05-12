f08e06e73e8d4f3ea67085ee09e07ef2061ed175
leiyihai <leiyihai@vip.qq.com>
2026-05-13 00:09:44 +0800

    完成第18课：闭包与迭代器（闭包语法/迭代器方法链/fold/sum/find）——进度 18/20

--- src/bin/ex18_closures.rs
+++ src/bin/ex18_closures.rs
@@ -14,15 +14,17 @@ fn main() {
     println!("5 + 10 = {}", add_x(5));
 
     // 写一个闭包 multiply，接受两个 i32，返回它们的乘积
+    let multiply = |a:i32, b:i32| return a * b;
+    println!("5 * 10 = {}", multiply(5, 10));
     // 写一个闭包 square，接受一个 i32，返回它的平方
-    // 提示：类型可以省略让编译器推断
+    let square = | s:i32| -> i32 { s * s };
 
-    // TODO
 
     println!("--- 任务 1 结束 ---");
 
     // ==================== 任务 2：将闭包传给函数 ====================
     // apply 函数已经写好，写一个闭包传给 apply
+
 
     fn apply<F>(f: F, value: i32) -> i32
     where
@@ -31,10 +33,11 @@ fn main() {
         f(value)
     }
 
+
     let triple = |n| n * 3;
     println!("triple(7) = {}", apply(triple, 7));
 
-    // TODO: 写一个闭包 square，传给 apply 测试
+    println!("{}", apply(square, 7));
 
     println!("--- 任务 2 结束 ---");
 
@@ -48,7 +51,8 @@ fn main() {
 
     // 试试 .iter().map().collect() 把每个元素乘以 10 收集到新 Vec
 
-    // TODO
+    let new_numbers = numbers.iter().map(|x| x * 10).collect::<Vec<i32>>();
+    println!("{:?}", new_numbers);
 
     println!("--- 任务 3 结束 ---");
 
@@ -58,7 +62,8 @@ fn main() {
 
     let nums = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-    // TODO
+    let new_nums = nums.iter().filter(|x| *x % 2 == 0).map(|x| x * 3).take(3).collect::<Vec<i32>>();
+    println!("{:?}", new_nums);
 
     println!("--- 任务 4 结束 ---");
 
@@ -68,15 +73,28 @@ fn main() {
 
     let data = vec![1, 2, 3, 4, 5];
 
-    // TODO
+    let sum: i32 = data.iter().sum();
+    println!("求和：{}", sum);
+    let fold: i32 = data.iter().fold(1, |acc, x| acc * x);
+    println!("累乘：{}", fold);
+    let any  = data.iter().any(|x| *x % 2 == 0);
+    println!("是否存在偶数：{}", any);
+    let all = data.iter().all(|x| *x > 0);
+    println!("是否全部正数：{}", all);
+    let first_even = data.iter().find(|x| **x % 2 == 0);
+    println!("第一个偶数：{:?}", first_even);
 
     println!("--- 任务 5 结束 ---");
 }
 
 // ==================== 思考题 ====================
 // 1. 闭包和函数有什么区别？什么时候用闭包？
+// 闭包可以捕获函数外的变量。使用到迭代器需要
 // 2. Fn、FnMut、FnOnce 有什么区别？
+// Fn只读捕获的变量，FnMut修复捕获的数量，FnOnce转移所有权
 // 3. 迭代器为什么是"零成本抽象"？
+// 不知道
 // 4. iter() 和 into_iter() 有什么区别？
+// iter()拿到的是&T, into_iter()拿到的是T?
 
 // 运行方法：cargo run --bin ex18_closures
